5530268dc6d39308eaf9390f1f97f0f21f08575b
mirage335 <spamfreemirage335@gmail.com>
2013-04-26 16:41:10 +0000

    Pronterface fan control, slic3r new version, automatic cooling, reduced retractions, improved glass bed compatibility, and more.

--- custom/.Slic3r/generateDefault.sh
+++ custom/.Slic3r/generateDefault.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cat ~/.Slic3r/print/Cheap.ini ~/.Slic3r/filament/Solid.ini ~/.Slic3r/printer/HacDC.ini > ~/.Slic3r/default.ini
+cat ~/.Slic3r/print/Cheap.ini ~/.Slic3r/filament/HacDC.ini ~/.Slic3r/printer/HacDC.ini > ~/.Slic3r/default.ini
